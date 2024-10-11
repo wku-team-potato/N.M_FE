e14850551b3f181d540b4acd6fe99a5d79706d20
gguip1 <51047947+gguip1@users.noreply.github.com>
2024-10-12 03:28:38 +0900

    Add Activities, Add Nav
    네비게이션 바 추가, 메인 액티비티, 로그인 액티비티, 비밀번호 찾기 프래그먼트 추가

--- app/src/main/java/com/example/application/LoginActivity.kt
+++ app/src/main/java/com/example/application/LoginActivity.kt
@@ -1,25 +1,20 @@
 package com.example.application
 
 import android.os.Bundle
-import android.view.Window
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_login)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
-
-        window.statusBarColor = ContextCompat.getColor(this, R.color.basic_background)
-        window.navigationBarColor = ContextCompat.getColor(this, R.color.white)
     }
 }
